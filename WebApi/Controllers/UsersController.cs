74d240ade0bf4b500daae0c94a340bce32b29f1d
Patrick <carty.patrick@gmail.com>
2020-04-27 05:01:04 +1000

    Started backend api with test controller

---
+++ WebApi/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Helpers;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private UserService _userService;
+
+        public UsersController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        [Route("TestNum")]
+        //GET: /api/Users/TestNum
+        public IActionResult GetTestNum()
+        {
+            var result = _userService.Test();
+
+            return Ok(result);
+        }
+    }
+}
---
+++ WebApi/Helpers/UserService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Data;
+using WebApi.Entities;
+
+namespace WebApi.Helpers
+{
+    public class UserService
+    {
+        private DataContext _context;
+
+        public UserService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public int Test()
+        {
+            return 3;
+        }
+    }
+}
