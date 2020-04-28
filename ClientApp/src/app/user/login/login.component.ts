ae79f56d16532e841cc80766f980e88f3954c503
Patrick <carty.patrick@gmail.com>
2020-04-29 01:26:25 +1000

    Front end UI can log in. Test alerts in place to verify correct user is logged in.

--- ClientApp/src/app/user/login/login.component.ts
+++ ClientApp/src/app/user/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+
+import { AuthService } from '../../_services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +11,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private authService: AuthService
+  ) {
+    //testing to see if user is already logged in
+    if (this.authService.currentUserValue) {
+      alert(this.authService.currentUserValue.username + ' is already logged in');
+    }
+   }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
   }
 
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      alert('There was a validation error with login');
+      return;
+    }
+
+    this.authService.login(
+      this.loginForm.controls['username'].value,
+      this.loginForm.controls['password'].value
+    )
+      .pipe(first())
+      .subscribe(
+        data => {
+          //test if login was a success
+          alert('Log in successful');
+          this.router.navigate(['/home']);
+        },
+        error => {
+          alert('There was a database error with login');
+        }
+      )
+  }
+
+  testCurrentUser() {
+    if (this.authService.currentUserValue) {
+      alert('Current user is: ' + this.authService.currentUserValue.username);
+    } else {
+      alert('No users currently logged in');
+    }
+  }
 }
